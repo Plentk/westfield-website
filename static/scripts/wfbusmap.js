feb90c0cb11855c4128e880a05f154b27f6bbf6c
Plentk <149661762+Plentk@users.noreply.github.com>
2026-07-30 15:21:29 +0800

    Update wfbusmap.js

--- static/scripts/wfbusmap.js
+++ static/scripts/wfbusmap.js
@@ -21,13 +21,13 @@ const toolTipMargin = 5;
 async function loadFiles() {
   try {
     let fetched;
-    fetched = await fetch("../../../static/db/roads.json");
+    fetched = await fetch("../../static/db/roads.json");
     wfroadsdata = await fetched.json();  
-    fetched = await fetch("../../../static/db/wfbusroute.json");
+    fetched = await fetch("../../static/db/wfbusroute.json");
     wfbusroutesdata = await fetched.json();
-    fetched = await fetch("../../../static/db/buildings.json");
+    fetched = await fetch("../../static/db/buildings.json");
     wfbuildingsdata = await fetched.json();
-    fetched = await fetch("../../../static/db/busstops.json");
+    fetched = await fetch("../../static/db/busstops.json");
     wfbusstopsdata = await fetched.json();
 
     drawRoads(wfroadsdata);
